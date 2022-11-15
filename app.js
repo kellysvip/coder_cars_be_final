0b114aa9dbdbb3e3ab8c31f8091d234bab5e856f
Vu Hoang <hoangitv6@gmail.com>
2022-11-15 17:22:30 +0700

    err hdl

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const cors = require('cors');
 const indexRouter = require('./routes/index');
 const mongoose = require('mongoose');
-const { sendResponse } = require('./helpers/utils');
+const { sendResponse, AppError } = require('./helpers/utils');
 require("dotenv").config();
 const app = express();
 
