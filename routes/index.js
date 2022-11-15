d143b7d9a85909263b392eb2c1d909b6586424e3
Vu Hoang <hoangitv6@gmail.com>
2022-11-15 15:41:28 +0700

    err hdl

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+/* GET home page. */
+router.get("/", function (req, res, next) {
+    res.status(200).send("Welcome to CoderSchool!");
+  });
 // CAR
 const carAPI = require('./car.api');
 router.use('/cars', carAPI);
